a665adc2910d988c1512e492c1cfa19ec913c1de
Victor <victoravelloso94@gmail.com>
2017-08-23 14:11:59 -0300

    A função convolution agora funciona com qualquer mask de duas dimenções.

--- hw1/p1-082674-???/src/convolution.py
+++ hw1/p1-082674-???/src/convolution.py
@@ -13,7 +13,8 @@ def convolution (img_input, mask):
 	h, l = k_heigh//2, k_length//2
 
 	# odd variable used so even kernels can work.
-	odd = k_heigh%2 # 0 if even, 1 if odd
+	hodd = k_heigh%2
+	lodd = k_length%2 
 
 	if(heigh < k_heigh or length < k_length):
 		return -1
@@ -22,7 +23,7 @@ def convolution (img_input, mask):
 	for i in range(heigh):
 		for j in range(length):
 			# get the sub-array
-			sub_img = img[max(0, i-h):min(heigh, i+h+odd), max(0, j-l):min(length, j+l+odd)]
+			sub_img = img[max(0, i-h):min(heigh, i+h+hodd), max(0, j-l):min(length, j+l+lodd)]
 			sub_h, sub_l, sub_rgb = sub_img.shape
 			if sub_h != k_heigh or sub_l != k_length: 
 				# if the sizes are different, it means it's near a border.
@@ -32,14 +33,14 @@ def convolution (img_input, mask):
 				if i-h < 0:
 					aux = np.flipud(sub_img[0:h-i,:])
 					sub_img = np.concatenate((aux, sub_img), axis=0)
-				if i+h+odd > heigh:
-					aux = np.flipud(sub_img[heigh-(i+h+odd):heigh,:])
+				if i+h+hodd > heigh:
+					aux = np.flipud(sub_img[heigh-(i+h+hodd):heigh,:])
 					sub_img = np.concatenate((sub_img,aux), axis=0)
 				if j-l < 0:
 					aux = np.fliplr(sub_img[:,0:l-j])
 					sub_img = np.concatenate((aux, sub_img), axis=1)
-				if j+l+odd > length:
-					aux = np.fliplr(sub_img[:,length-(j+l+odd):length])
+				if j+l+lodd > length:
+					aux = np.fliplr(sub_img[:,length-(j+l+lodd):length])
 					sub_img = np.concatenate((sub_img,aux), axis=1)
 			if(sub_img.shape[0] != k_heigh or sub_img.shape[1] != k_length):
 				# checks to see if the img was padded correctly
@@ -53,25 +54,36 @@ def convolution (img_input, mask):
 
 img = cv2.imread('../input/input-p1-2-1-0.png', 1)
 
-#Kernel 3x3
-msk = np.full((3,3), 1/(3*3))
+#Kernel 3x3 edge detection
+msk = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
 img_conv = convolution (img, msk)
 cv2.imwrite('../output/output-p1-2-1-0.png', img_conv)
-#Kernel 7x7
+
+#Kernel 7x7 box blur
 msk = np.full((7,7), 1/(7*7))
 img_conv = convolution (img, msk)
 cv2.imwrite('../output/output-p1-2-1-1.png', img_conv)
-#Kernel 15x15
+
+#Kernel 15x15 box blur
 msk = np.full((15,15), 1/(15*15))
 img_conv = convolution (img, msk)
 cv2.imwrite('../output/output-p1-2-1-2.png', img_conv)
 
-#Kernel 50x50
+#Kernel 50x50 box blur
 msk = np.full((50,50), 1/(50*50))
 img_conv = convolution (img, msk)
 cv2.imwrite('../output/output-p1-2-1-3.png', img_conv)
 
 
+#Testes com kernels de formas diferentes, nao é necesario para o programa final
+#msk = np.array([[-0.5, 0, 0.5]])
+#img_conv = convolution (img, msk)
+#cv2.imwrite('../output/output-p1-2-1-teste-0.png', img_conv)
 
+#msk = np.array([[0.6]])
+#img_conv = convolution (img, msk)
+#cv2.imwrite('../output/output-p1-2-1-teste-01.png', img_conv)
 
-
+#msk = np.array([[-1, -1, -1], [-1, 5, -1]])
+#img_conv = convolution (img, msk)
+#cv2.imwrite('../output/output-p1-2-1--teste-02.png', img_conv)
