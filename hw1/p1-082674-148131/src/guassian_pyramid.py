20229468089d6be3170af5c9e984dc2729f3accf
Victor <victoravelloso94@gmail.com>
2017-08-25 22:28:55 -0300

    Piramide já esta funcionando, porém ainda falta achar a melhor gausian mask para up, e precisa interpolar os valores de down

--- hw1/p1-082674-148131/src/guassian_pyramid.py
+++ hw1/p1-082674-148131/src/guassian_pyramid.py
@@ -1,6 +1,7 @@
 import convolution as conv
 import cv2
 import numpy as np
+import sys
 
 class GPyramid:
 	images = []
@@ -8,31 +9,54 @@ class GPyramid:
 	def down(self, image):
 		# Desce um nivel na piramide
 		# Duplica linhas e colunas
-		# Interpola usando a função convolution 'conv.convolution(img, msk)'
-		# return a imagem nova	
-		return
+		image_down = np.repeat(image, 2, axis=0)
+		image_down = np.repeat(image_down, 2, axis=1)
+
+		# Interpolar
+		# Pode utilizar uma fucao de biblioteca nesse caso(prof. confimou no moodle)
+
+		return image_down
 
 	def up(self, image):
-		# Sobe um nivel na piramide
-		# Blur usando uma gaussian mask e a função convolution 'conv.convolution(img, msk)'
+		# Gaussian mask, eu escolhi essa mascara só para testar a piramide, 
+		# mas sera necessario testar varias masks 
+		# ou encontrar uma mask que conseguimos explicar pq é a melhor.
+		msk = np.array([[1, 4, 7, 4, 1],
+						[4,16,26,16, 4],
+						[7,26,41,26, 7],
+						[4,16,26,16, 4],
+						[1, 4, 7, 4, 1]])*1/273
+		# Blur
+		image_up = conv.convolution(image, msk)
 		# Remove linhas e colunas pares
-		# return a imagem nova
-		return image
+		image_up = image_up[::2,::2]
+		return image_up
 
 	def access(self, level):
-		if (images.shape > level):
-			return images[level]
+		if(level < 0):
+			print("Level value must be positive.\n")
+			return None
+		if (len(self.images) > level):
+			return self.images[level]
 		else:
-			# Encontra o level utilizando up/down
-			return
+			for i in range(len(self.images)-1, level):
+				self.images.append(self.up(self.images[i]))
+			return self.images[level]
 
 	def __init__(self, image, levels):
 		self.images.append(image)
 		for i in range(levels):
 			self.images.append(self.up(self.images[i]))
 
+img = cv2.imread('../input/input-p1-2-2-0.jpg', 1)
+if img is None:
+	print("Image not found.")
+	sys.exit()
+G = GPyramid(img, 2)
+#Down-scaling(Subindo na piramide)
+cv2.imwrite('../output/output-p1-2-2-0.png', G.access(1))
+cv2.imwrite('../output/output-p1-2-2-1.png', G.access(2))
+#Upscaling(DEscendo na piramide)
+cv2.imwrite('../output/output-p1-2-2-3.png', G.down(G.images[1]))
+cv2.imwrite('../output/output-p1-2-2-4.png', G.down(G.images[2]))
 
-img = cv2.imread('../input/input-p1-2-1-0.png', 1)
-G = GPyramid(img, 3)
-print(G.images[0].shape, G.images[1].shape, G.images[2].shape)
-		
