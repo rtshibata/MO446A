1cba0d2a4aaa66ef54fef8a3ed0ed38a0d1f38c9
Victor <victoravelloso94@gmail.com>
2017-08-29 16:08:34 -0300

    Imagens e mudança na leitura das imagens

--- hw1/p1-082674-148131/src/gp_test.py
+++ hw1/p1-082674-148131/src/gp_test.py
@@ -2,16 +2,19 @@ import gaussian_pyramid as gp
 import cv2
 import numpy as np
 import sys
+import os
 
-img = cv2.imread('../input/input-p1-2-2-0.jpg', 1)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+img = cv2.imread(dir_path+'/../input/input-p1-2-2-0.png', 1)
 if img is None:
 	print("Image not found.")
 	sys.exit()
 G = gp.GPyramid(img, 4)
 #Down-scaling(Subindo na piramide)
-cv2.imwrite('../output/output-p1-2-2-0.png', G.access(2))
-cv2.imwrite('../output/output-p1-2-2-1.png', G.access(3))
-cv2.imwrite('../output/output-p1-2-2-2.png', G.access(4))
+cv2.imwrite(dir_path+'/../output/output-p1-2-2-0.png', G.access(1))
+cv2.imwrite(dir_path+'/../output/output-p1-2-2-1.png', G.access(2))
+cv2.imwrite(dir_path+'/../output/output-p1-2-2-2.png', G.access(3))
+cv2.imwrite(dir_path+'/../output/output-p1-2-2-3.png', G.access(4))
 #Upscaling(DEscendo na piramide)
-cv2.imwrite('../output/output-p1-2-2-3.png', G.down(G.access(3)))
+cv2.imwrite(dir_path+'/../output/output-p1-2-2-4.png', G.down(G.access(1)))
 
