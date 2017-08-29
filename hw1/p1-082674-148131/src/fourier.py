7a888fdffd0e61b3559dead7a047df0dffb84b6b
Victor <victoravelloso94@gmail.com>
2017-08-29 19:10:12 -0300

    Makefile e mudança no input/output de fourier

--- hw1/p1-082674-148131/src/fourier.py
+++ hw1/p1-082674-148131/src/fourier.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import os
 
-img = cv2.imread('../input/input-p1-3-1-0.jpg',0)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+img = cv2.imread(dir_path+'/../input/input-p1-3-1-0.jpg',0)
 
 dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
@@ -11,5 +12,5 @@ magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
 phase_spectrum = 40*np.log(cv2.phase(dft_shift[:, :, 0], dft_shift[:, :, 1], True))
 
-cv2.imwrite('../output/output-p1-3-1-0.png', magnitude_spectrum)
-cv2.imwrite('../output/output-p1-3-1-1.png', phase_spectrum)
+cv2.imwrite(dir_path+'/../output/output-p1-3-1-0.png', magnitude_spectrum)
+cv2.imwrite(dir_path+'/../output/output-p1-3-1-1.png', phase_spectrum)
