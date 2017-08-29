6927dbc1e7266fc3074609baaea01ce6b38bf52c
Renato Shibata <rtshibata@gmail.com>
2017-08-29 17:07:24 -0300

    fourier com erros...

--- hw1/p1-082674-148131/src/fourier.py
+++ hw1/p1-082674-148131/src/fourier.py
@@ -2,21 +2,31 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+def find_nth_smallest_proper_way(a, n):
+    return np.partition(a, n-1)[n-1]
+
 def spectrum_lowest_values(spectrum,percentage=None):
 	"""
 	selects the n% of the lowest values in phase spectrum
 	(bigger than 0)
 	"""
+	#no_zeros_array = np.trim_zeros(spectrum.flatten())
+	array = spectrum.flatten()
+	
 	if percentage is None:
-		#no_zeros_array = np.trim_zeros(spectrum.flatten())
 
 		#get the lowest value which is different of 0 
 		minval = np.min(spectrum[np.nonzero(spectrum)])
-		#makes every other point become equal to 0
-		spectrum[spectrum > minval] = 0
+		
+	else:
+		#get what is the n-th lowest value according to the percentage
+	 	nth = int((array.shape[0]*percentage)/100)
+		#find n-th lowest value
+		minval = find_nth_smallest_proper_way(array, nth)		
 	
-	#else:
-		#selects the percentage% of lowest values
+	#makes every other point become equal to 0
+	spectrum[spectrum > minval] = 0
+	
 	return spectrum
 
 img = cv2.imread('../input/input-p1-3-1-0.jpg',0)
@@ -40,3 +50,35 @@ f_ishift = np.fft.ifftshift(f_spec)
 img_back = cv2.idft(f_ishift)
 
 cv2.imwrite('../output/output-p1-3-1-2.png', img_back)
+
+#takes 25% of the lowest values
+f_spec = spectrum_lowest_values(phase_spectrum,25)
+
+f_ishift = np.fft.ifftshift(f_spec)
+img_back = cv2.idft(f_ishift)
+
+cv2.imwrite('../output/output-p1-3-1-3.png', img_back)
+
+#takes 50% of the lowest values
+f_spec = spectrum_lowest_values(phase_spectrum,50)
+
+f_ishift = np.fft.ifftshift(f_spec)
+img_back = cv2.idft(f_ishift)
+
+cv2.imwrite('../output/output-p1-3-1-4.png', img_back)
+
+#takes 75% of the lowest values
+f_spec = spectrum_lowest_values(phase_spectrum,75)
+
+f_ishift = np.fft.ifftshift(f_spec)
+img_back = cv2.idft(f_ishift)
+
+cv2.imwrite('../output/output-p1-3-1-5.png', img_back)
+
+#takes 100% of the lowest values
+f_spec = spectrum_lowest_values(phase_spectrum,100)
+
+f_ishift = np.fft.ifftshift(f_spec)
+img_back = cv2.idft(f_ishift)
+
+cv2.imwrite('../output/output-p1-3-1-6.png', img_back)
