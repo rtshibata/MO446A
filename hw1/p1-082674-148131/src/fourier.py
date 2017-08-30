74c9c47bd21c727175faa43ccfa5a8f8ab356f34
Renato Shibata <rtshibata@gmail.com>
2017-08-30 15:23:03 -0300

    fourier ok

--- hw1/p1-082674-148131/src/fourier.py
+++ hw1/p1-082674-148131/src/fourier.py
@@ -2,11 +2,22 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-#fill every element greater than minval as 0
-def equalize_rest_0(m,minval):
+def equalize_rest_0(m,val):
+	"""
+	fill every element greater than val as 0
+	"""
 	# matriz[i][j] = 0, if m[i][j] > minval; 
 	# matriz[i][j] = m[i][j], else.
-	matrix = np.where(m > minval, 0, m)
+	matrix = np.where(m > val, 0, m)
+	return matrix
+
+def equalize_rest_0_bigger(m,val):
+	"""
+	fill every element smaller than val as 0
+	"""
+	# matriz[i][j] = 0, if m[i][j] > minval; 
+	# matriz[i][j] = m[i][j], else.
+	matrix = np.where(m < val, 0, m)
 	return matrix
 
 def find_nth_biggest(a, n):
@@ -18,6 +29,31 @@ def find_nth_smallest(a, n):
     #sort the elements in the array a
 	result = sorted(a)
 	return result[n-1]
+
+def spectrum_biggest_values(dft,percentage=None):
+	"""
+	selects the n% of the biggest values in phase spectrum
+	(bigger than 0)
+	"""
+	#no_zeros_array = np.trim_zeros(spectrum.flatten())
+	array = dft.flatten()
+	
+	if percentage is None:
+		#get the biggest value which is different of 0 
+		maxval = np.max(dft[np.nonzero(dft)])
+		print maxval				
+	else:
+		#get what is the n-th biggest value according to the percentage
+	 	nth = int((array.shape[0]*percentage)/100)
+		#find n-th biggest value
+		maxval = find_nth_biggest(array, nth)
+		print maxval		
+	
+	#makes every other point become equal to 0
+	result = equalize_rest_0_bigger(dft,maxval)	
+	#dft[dft > minval] = 0
+	
+	return result
 
 def spectrum_lowest_values(dft,percentage=None):
 	"""
@@ -44,8 +80,18 @@ def spectrum_lowest_values(dft,percentage=None):
 	
 	return result
 
-def idft_output(dft_shift,percentage=None):
+def idft_output_min(dft_shift,percentage=None):
 	f_spec = spectrum_lowest_values(dft_shift,percentage)
+
+	f_ishift = np.fft.ifftshift(f_spec)
+	img_back = cv2.idft(f_ishift, flags=cv2.DFT_SCALE)
+	magnitude_spectrum = (cv2.magnitude(img_back[:,:,0],img_back[:,:,1]))
+	phase_spectrum = (cv2.phase(img_back[:, :, 0], img_back[:, :, 1], True))
+
+	return phase_spectrum, magnitude_spectrum
+
+def idft_output_max(dft_shift,percentage=None):
+	f_spec = spectrum_biggest_values(dft_shift,percentage)
 
 	f_ishift = np.fft.ifftshift(f_spec)
 	img_back = cv2.idft(f_ishift, flags=cv2.DFT_SCALE)
@@ -68,26 +114,53 @@ cv2.imwrite('../output/output-p1-3-1-0.png', magnitude_spectrum)
 cv2.imwrite('../output/output-p1-3-1-1.png', phase_spectrum)
 
 #takes only 1 point, the lowest value
-spectrums = idft_output(dft_shift)
+spectrums = idft_output_min(dft_shift)
 cv2.imwrite('../output/output-p1-3-1-2.png', spectrums[0])
 cv2.imwrite('../output/output-p1-3-1-7.png', spectrums[1])
 
 #takes 25% of the lowest values
-spectrums = idft_output(dft_shift,25)
+spectrums = idft_output_min(dft_shift,25)
 cv2.imwrite('../output/output-p1-3-1-3.png', spectrums[0])
 cv2.imwrite('../output/output-p1-3-1-8.png', spectrums[1])
 
 #takes 50% of the lowest values
-spectrums = idft_output(dft_shift,50)
+spectrums = idft_output_min(dft_shift,50)
 cv2.imwrite('../output/output-p1-3-1-4.png', spectrums[0])
 cv2.imwrite('../output/output-p1-3-1-9.png', spectrums[1])
 
 #takes 75% of the lowest values
-spectrums = idft_output(dft_shift,75)
+spectrums = idft_output_min(dft_shift,75)
 cv2.imwrite('../output/output-p1-3-1-5.png', spectrums[0])
 cv2.imwrite('../output/output-p1-3-1-10.png', spectrums[1])
 
 #takes 100% of the lowest values
-spectrums = idft_output(dft_shift,100)
+spectrums = idft_output_min(dft_shift,100)
 cv2.imwrite('../output/output-p1-3-1-6.png', spectrums[0])
 cv2.imwrite('../output/output-p1-3-1-11.png', spectrums[1])
+
+#takes only 1 point, the highest value
+spectrums = idft_output_max(dft_shift)
+cv2.imwrite('../output/output-p1-3-1-12.png', spectrums[0])
+cv2.imwrite('../output/output-p1-3-1-17.png', spectrums[1])
+
+#takes 25% of the highest values
+spectrums = idft_output_max(dft_shift,25)
+cv2.imwrite('../output/output-p1-3-1-13.png', spectrums[0])
+cv2.imwrite('../output/output-p1-3-1-18.png', spectrums[1])
+
+#takes 50% of the highst values
+spectrums = idft_output_max(dft_shift,50)
+cv2.imwrite('../output/output-p1-3-1-14.png', spectrums[0])
+cv2.imwrite('../output/output-p1-3-1-19.png', spectrums[1])
+
+#takes 75% of the highest values
+spectrums = idft_output_max(dft_shift,75)
+cv2.imwrite('../output/output-p1-3-1-15.png', spectrums[0])
+cv2.imwrite('../output/output-p1-3-1-20.png', spectrums[1])
+
+#takes 100% of the highest values
+spectrums = idft_output_max(dft_shift,100)
+cv2.imwrite('../output/output-p1-3-1-16.png', spectrums[0])
+cv2.imwrite('../output/output-p1-3-1-21.png', spectrums[1])
+
+
