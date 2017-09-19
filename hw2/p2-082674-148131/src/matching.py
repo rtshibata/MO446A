44f2cfae530ca45b04f7b25e816349348d680e05
Victor <victoravelloso94@gmail.com>
2017-09-19 19:36:16 -0300

    Matching funcionando e pequenas mudanças no sift (agora tem methods para pegar uma list de keypoint inteiro e uma numpy array para descriptors)

--- hw2/p2-082674-148131/src/matching.py
+++ hw2/p2-082674-148131/src/matching.py
@@ -1,4 +1,3 @@
-import sift as s
 import cv2
 import numpy as np
 import os
@@ -7,22 +6,23 @@ from scipy.spatial import distance
 
 #usa distancia euclidiana para comparar os descritores
 def cmp_desc(desc1, desc2):
-	return distance.euclidian(desc1,desc2)
+	return distance.euclidean(desc1,desc2)
 	
 #tem q depois atualizar para excluir/atualizar a lista dos descritores a cd iteracao
-def find_matches(sift1, sift2,t):
-	dist_min = math.inf
-	i_min = math.inf
+def find_matches(descriptor1, descriptor2,t):
 	pts_matches = []
-	list1 = sift1.desc_list[0][0]
-	list2 = sift2.desc_list[0][0]
-	for i_desc1 in len(sift1.desc_list[0][0]):
-		for i_desc2 in len(sift2.desc_list[0][0]):
-			d = cmp_desc(sift1.desc_list[0][0][i_desc1],sift1.desc_list[0][0][i_desc2]) 
-			if d < dist_min and d < t:
-				dist_min = d
-				i_min = i_desc2
+	chosen_list = [False for i in range(descriptor2.shape[0])]
+	for i_desc1 in range(descriptor1.shape[0]):
+		dist_min = math.inf
+		i_min = math.inf
+		for i_desc2 in range(descriptor2.shape[0]):
+			if chosen_list[i_desc2] == False:
+				d = cmp_desc(descriptor1[i_desc1,:], descriptor2[i_desc2,:]) 
+				if d < dist_min and d < t:
+					dist_min = d
+					i_min = i_desc2
 		if i_min != math.inf:
+			chosen_list[i_min] = True
 			pair = (i_desc1, i_min)
 			pts_matches.append(pair)
 	return pts_matches
