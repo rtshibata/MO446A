e831341c74073eb8d79c026b98ae6073285316f4
Renato Toshiaki Shibata <ra082674@lawrence.lab.ic.unicamp.br>
2017-09-19 17:52:53 -0300

    ransac implementado junto com affine transform

--- hw2/p2-082674-148131/src/sift_test.py
+++ hw2/p2-082674-148131/src/sift_test.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import math
 import matching as m
+import random
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 img = cv2.imread(dir_path+'/../input/input-p2-3-1-0.jpg', 0)
@@ -88,8 +89,54 @@ print(len(sift2.desc_list[0][0]))
 
 #match hypothesis
 n_pixels,_ = sift1.dog[0][0].shape
-t_semelhanca = n_pixels/2
-list_points = m.find_matches(sift1,sift2,t_semelhanca)
-print(list_points)
-#RANSAC
+t_semelhanca = n_pixels//2
+list_index_desc = m.find_matches(sift1,sift2,t_semelhanca)
+#print(list_index_desc)
 
+#RANSAC
+n_ransac = len(list_index_desc)//3 #3 sem criterio
+threshold = 10 #numero sem criterio
+good_model =  len(list_index_desc)//2 #mais de 50% ok
+maybe_model = []
+consensus_set = []
+list3p= []
+best_error = math.inf
+
+for count in range(n_ransac)
+	list3p = list_index_desc[3*count:3*(count+1)]
+	consensus_set = list3p
+	not_inliers = list_index_desc[:3*count] + list_index_desc[3*(count+1):]
+	i1, j1 = list3p[0]
+	ponto1 = sift1.key_points.struct[i1].pt
+	ponto1_ = sift2.key_points.struct[j1].pt
+	i2, j2 = list3p[1]
+	ponto2 = sift1.key_points.struct[i2].pt
+	ponto2_ = sift2.key_points.struct[j2].pt
+	i3, j3 = list3p[2]
+	ponto3 = sift1.key_points.struct[i3].pt
+	ponto3_ = sift2.key_points.struct[j3].pt
+	#selects possible inliers	
+	list3p = [ponto1,ponto2,ponto3,ponto1_,ponto2_,ponto3_]
+	#affine transformation
+	a = AffineTransf(list3p)
+	#gets possible model
+	maybe_model = a.getA()
+	## qdo tiver menos de 3 descritores em not_inliers, cancelar o teste de erro
+	## !!!!!!!!!
+	###!!!!!!!!
+	for index_desc in not_inliers:
+		if a.error(list_index_desc) < t:
+			consensus_set.append(index_desc)
+	
+	if len(consensus_set) > good_model:
+		this_model = maybe_model
+		sample1= randrange(0,len(consensus_set))
+		sample2= randrange(0,len(consensus_set))
+		sample3= randrange(0,len(consensus_set))
+		list_sample = [sample1,sample2,sample3]
+		this_error = a.error(list_sample)
+		if this_error < best_error:
+			best_model = this_model
+			best_consensus_set = consensus_set
+			best_error = this_error		
+	#https://www.cse.buffalo.edu/~jryde/lectures/cse410/MobileRobotMapping_2.html
--- hw2/p2-082674-148131/src/transform.py
+++ hw2/p2-082674-148131/src/transform.py
@@ -4,19 +4,45 @@ import math
 
 class AffineTransf:	
 	def __init__(self, list_3pt_matches):
-		if len(list_3pt_matches) != 3:
-			print("3 point matches required")
+		if len(list_3pt_matches) != 6:
+			print("3 points of each image required")
 			return -1
 		for pt_match in list_3pt_matches:
 			if not isinstance(pt_match, tuple):
 				print("point matches must be tuples")
 				return -1
 		
+		x1 , y1 = list_3pt_matches[0]
+		x1_, y1_ = list_3pt_matches[3]	
+		x2 , y2 = list_3pt_matches[1]
+		x2_, y2_ = list_3pt_matches[4]	
+		x3 , y3 = list_3pt_matches[2]
+		x3_, y3_ = list_3pt_matches[5]	
+		X = np.matrix([x1, y1, 1, 0, 0, 0],
+					  [0, 0, 0, x1, y1, 1],
+  					  [x2, y2, 1, 0, 0, 0],
+  					  [0, 0, 0, x2, y2, 1],
+  					  [x3, y3, 1, 0, 0, 0],
+  					  [0, 0, 0, x3, y3, 1])
 		
-		x1=		
-		X = np.matrix([ ]
+		Y = np.matrix([x1_,y1_,x2_,y2_,x3_,y3_])
+		Xt = X.transpose()
+		product1 = np.dot(Xt,X)
+		inverse1 = np.linalg(product1)
+		product2 = np.dot(Xt,Y)
+		A = np.dot(inverse1,product2)
+		self.A = A
+
+		
+		def getA(self):
+			return self.A
+		
+		#calcula a diferenca entre XA e Y esperado: XA - Y = erro
+		def error(self,list_i_desc):
+			''' matriz X é definido pelos 3 pontos associados 
+			aos indices do list_i_desc
 			
+			em list_i_desc ha indices de 3 pontos associados (x1,y1)
+			(x2,y2),(x3,y3) alem dos (x1_,y1_),(x2_,y2_),(x3_,y3_)
 			
-				)
-				
-		self.A = (np.linalg(transpose(X)*X))*(transpose(X)*Y) 
+			'''
